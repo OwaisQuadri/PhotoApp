936a5ada474025a911a8119fd913aa962ecac7f5
Owais Quadri <owaisquadri01@gmail.com>
2023-07-07 02:59:44 -0400

    tear down sut and form

--- PhotoAppTests/Signup/Network/SignupWebServiceTests.swift
+++ PhotoAppTests/Signup/Network/SignupWebServiceTests.swift
@@ -21,6 +21,8 @@ final class SignupWebServiceTests: XCTestCase {
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        form = nil
     }
     
     func testSignupWebService_GivenSuccessResponse_ReturnSuccess() {
