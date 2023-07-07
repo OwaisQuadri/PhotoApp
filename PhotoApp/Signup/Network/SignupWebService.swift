4f18a27ba3cb922d782d37877d30dc0ec404d0ef
Owais Quadri <owaisquadri01@gmail.com>
2023-07-07 01:54:30 -0400

    allow urlSession to be mocked later on

--- PhotoApp/Signup/Network/SignupWebService.swift
+++ PhotoApp/Signup/Network/SignupWebService.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 class SignupWebService {
-    var url: URL?
-    init(urlString: String) {
+    private var urlSession: URLSession
+    private var url: URL?
+    init(urlString: String, urlSession: URLSession = .shared) {
         url = URL(string: urlString)
+        self.urlSession = urlSession
     }
     func register(with form: SignupFormRequestModel, completion: @escaping (SignupResponseModel?, SignupError?) -> Void) {
         guard let url = url else { return } // TODO: create unit test for invalid URL and throw an error
@@ -20,7 +22,7 @@ class SignupWebService {
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         request.httpBody = try? JSONEncoder().encode(form)
         
-        URLSession.shared.dataTask(with: request) { (data, response, err) in
+        urlSession.dataTask(with: request) { (data, response, err) in
             //
         }.resume()
         
